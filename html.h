1625047e515cdb678ed83fbed281208765466cb3
Thomas ALLARD <thomasallard@free.fr>
2017-03-16 14:44:15 +0100

    Minify IHM

--- html.h
+++ html.h
@@ -3,8 +3,10 @@ extern const char* html_index = "\
 <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1'>\
 <title>IoT - Rythm'n Hits</title><style type='text/css'>body{padding-top: 50px;}.content{padding: 40px 15px; text-align: center;}</style></head>\
 <body><nav class='navbar navbar-inverse navbar-fixed-top'> <div class='container'> <div class='navbar-header'> <a class='navbar-brand' href='#'>Rythm'n Hits</a> </div>\
-</div></nav><div class='container'><div class='content'><h1>IoT Project - Rythm'n Hits</h1><br><br><h3>Actual BPM: <span id='actualBPM'></span></h3><h3>Update BPM</h3>\
-<form method='get' action='http://10.33.0.77/setbpm' target=':blank'><br><input type='number' name='bpm'><br><input type='submit' value='Update BPM'></form></div>\
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>\
-<script>// get$.get('http://10.33.0.77/getbpm', function (data){$('#actualBPM').html(data);});</script></body></html>\
+</div></nav><div class='container'><div class='content'><h1>IoT Project - Rythm'n Hits</h1><br><br><h3 class='actual'>Actual BPM: <span id='actualBPM'></span></h3>\
+<form method='get' action='/setbpm' id='updateBPMForm' class='form-inline'><input type='number' id='inputBpm' class='form-control mb-2 mr-sm-2 mb-sm-0' name='bpm'>\
+<input type='submit' class='btn btn-primary' value='Update BPM'></form></div></div><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>\
+<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>\
+<script>$(document).ready(function(){getCurrentBPM();$('#updateBPMForm').submit(function(form){form.preventDefault();$.ajax({url: '/setbpm',type: 'get',data: $('#updateBPMForm').serialize(),success: function(){getCurrentBPM();}});});});\
+function getCurrentBPM(){$.get('/getbpm', function (data){$('#actualBPM').html(data);$('#inputBpm').val(data);});}</script></body></html>
 ";
