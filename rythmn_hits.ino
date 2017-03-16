419914cae01c3bbece54c90bdf7479aa6e68e210
Anthony Rey <anthony.rey5@gmail.com>
2017-03-16 10:52:02 +0100

    Externalized the html to html.h

--- rythmn_hits.ino
+++ rythmn_hits.ino
@@ -5,6 +5,7 @@
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <ESP8266mDNS.h>          //Allow custom URL
+#include "html.h"
 
 void setupServer();
 void setupWifi();
@@ -13,17 +14,6 @@ void flash(int pin);
 void handleRoot();
 void setBpm();
 void getBpm();
-
-char* html_index = "\
-<!DOCTYPE html><html><head><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'><meta charset='utf-8'>\
-<meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1'>\
-<title>IoT - Rythm'n Hits</title><style type='text/css'>body{padding-top: 50px;}.content{padding: 40px 15px; text-align: center;}</style></head>\
-<body><nav class='navbar navbar-inverse navbar-fixed-top'> <div class='container'> <div class='navbar-header'> <a class='navbar-brand' href='#'>Rythm'n Hits</a> </div>\
-</div></nav><div class='container'><div class='content'><h1>IoT Project - Rythm'n Hits</h1><br><br><h3>Actual BPM: <span id='actualBPM'></span></h3><h3>Update BPM</h3>\
-<form method='get' action='http://10.33.0.77/setbpm' target=':blank'><br><input type='number' name='bpm'><br><input type='submit' value='Update BPM'></form></div>\
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>\
-<script>// get$.get('http://10.33.0.77/getbpm', function (data){$('#actualBPM').html(data);});</script></body></html>\
-";
 
 ESP8266WebServer server(80);
 const char *ssid = "metronome_server";
