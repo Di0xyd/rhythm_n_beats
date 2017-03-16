47e9fdc48cb3a5cbc295846e2be13002ed54e4ff
Jiiva <kalanithi.jeevanath@gmail.com>
2017-03-16 13:33:20 +0100

    insert beat_check

--- rythmn_hits.ino
+++ rythmn_hits.ino
@@ -14,10 +14,13 @@ void flash(int pin);
 void handleRoot();
 void setBpm();
 void getBpm();
+bool beatCheck();
 
 ESP8266WebServer server(80);
 const char *ssid = "metronome_server";
 
+long goodBeat = 0;
+long badBeat = 0;
 long bpm = 60;
 long beatCounter = 0; // % 4 == 0 = blue (premier temps) / sinon autre-
 const long msCountIn1Min = 60000;
@@ -28,11 +31,12 @@ long lastTime = 0;
 void setup() {
 
   Serial.begin(9600);
-  pinMode(A0, INPUT);
+  pinMode(D0, INPUT);
   pinMode(D5, OUTPUT);
   pinMode(D4, OUTPUT);
   pinMode(D2, OUTPUT);
-
+  pinMode(D1, OUTPUT);
+  pinMode(D3, OUTPUT);
   Serial.println("Starting WiFi.");
   setupWifi();
   setupServer();
@@ -51,18 +55,18 @@ void handleRoot() {
 void setBpm() {
   String bpm_str;
   if ( server.hasArg("bpm") ) {
-    
+
     bpm_str = server.arg(0);
     //add function to init
     long old_bpm = bpm;
     bpm = bpm_str.toInt();
-    
+
     if (bpm <= 0 || bpm > 250)  {
       bpm = old_bpm;
       server.send(400, "text/html", "error");
       return;
     }
-    
+
     waitBtwNotes = msCountIn1Min / bpm;
   }
 
@@ -77,8 +81,8 @@ void setupWifi() {
 
   WiFiManager wifiManager;
   //wifiManager.resetSettings();
-  wifiManager.setAPCallback([](WiFiManager* manager){});
- 
+  wifiManager.setAPCallback([](WiFiManager * manager) {});
+
   wifiManager.autoConnect(ssid);
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
@@ -94,27 +98,52 @@ void setupServer() {
 
 void metronome() {
   if ((millis() - lastTime) >= waitBtwNotes) {
+
     lastTime = millis();
 
     if (beatCounter % 4 == 0) {
       tone(D2, 440, 50);
-      flash(D4);
+      digitalWrite(D4, HIGH);
     }
     else {
       tone(D2, 300, 50);
-      flash(D5);
+      digitalWrite(D5, HIGH);
     }
+
+    if (beatCheck()) {
+      Serial.println(2000);
+      goodBeat++;
+    }
+
+    digitalWrite(D5, LOW);
+    digitalWrite(D4, LOW);
 
     beatCounter++;
     if (beatCounter > 3)
       beatCounter = 0;
   }
+
+  if (beatCheck()) {
+    Serial.println(1000);
+    badBeat++;
+  }
+  Serial.println(1);
+
 }
 
 void flash(int pin) {
   digitalWrite(pin, HIGH);
   delay(20);
   digitalWrite(pin, LOW);
+}
+
+bool beatCheck() {
+  long currentTime = millis();
+  long sensorValue = 0;
+  while (millis() <= currentTime + 50) {
+    sensorValue += digitalRead(D0);
+  }
+  return sensorValue > 0;
 }
 
 
